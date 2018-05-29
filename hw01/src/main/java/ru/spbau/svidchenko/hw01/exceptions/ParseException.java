08ed5fbe426c9a5eb79978310e9e13d2bfadf254
ArgentumWalker <svidchen-ko@mail.ru>
2018-05-29 16:27:05 +0300

    add missed javadoc

--- hw01/src/main/java/ru/spbau/svidchenko/hw01/exceptions/ParseException.java
+++ hw01/src/main/java/ru/spbau/svidchenko/hw01/exceptions/ParseException.java
@@ -1,5 +1,8 @@
 package ru.spbau.svidchenko.hw01.exceptions;
 
+/**
+ * Statement not parsed by any reason
+ */
 public class ParseException extends CliException {
     @Override
     public String getMessage() {
